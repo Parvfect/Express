4ea8901c2cda81a7ee0fca244c74d7b84b50ee2e
Parvfect <parvagrw02@gmail.com>
2020-12-27 11:27:31 +0000

    I was close but I need a break

--- index.js
+++ index.js
@@ -6,11 +6,46 @@
  */
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
+const app = express();
 
-MongoClient.connect('mongodb-connection-string', (err, client) => {
-    /** Do something here */
+/** Connection string to connect to mongodb (leave empty when pushing to git!!) */
+const connectionstring = '';
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/** Connecting to mongodb */
+MongoClient.connect(connectionstring, {
+    useUnifiedTopology : true
+}, (err, client) => {
+    if(err) return console.error(err);
+    console.log("Connection to database secured!");
+    
+    /** Naming the database */
+    const db = client.db('berd');
+    const Users = db.collection('Users');
+    
+    Users.insertOne("hola?")
+
+    /** We need the db variable from the connection to to access MongoDB.
+     *  This means we need to put our express request handlers into the MongoClient’s then call. */
+    
+     /** 
+    app.post('/new_user', (req, res) => {
+        Users.insertOne(req.body)
+        .then(result => {
+            console.log(result)
+        })
+        .catch(error => console.error(error))
+    })
+    */
+
+    //app.use();
+    //app.get();
+    //app.post();
+    //app.listen();
 });
 
 
@@ -18,10 +53,8 @@ MongoClient.connect('mongodb-connection-string', (err, client) => {
  * App Variables
  */
 
-const app = express();
-
 /** If available process.env.Port else 8000 */
-const port = "8001";
+const port = "8003";
 
 /**
  *  App Configurations - analogous to settings.py
@@ -51,6 +84,13 @@ app.get("/impossibleList", (req, res) => {
     /** Basically controls the page rendered and its header */
     res.render("impossibleList", { title: "Impossible List" });
 });
+
+
+
+app.post('/new_user', (req, res) => {
+    console.log("Helllooo new user!");    
+    res.redirect("/imp")
+})
 
 
 /**
